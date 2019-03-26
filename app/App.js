ef3efad860b7b9224ca33f85632b4ed61098abf9
RANJAN <roybellnet@gmail.com>
2019-03-26 21:28:35 +0530

    Updated file structure as per needs

---
+++ app/App.js
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router } from "react-router-dom";
+import { withRouter } from 'react-router';
+import './styles/app.css';
+import { requestRedirect } from '../lib/infra/actions'
+import Cookies from 'js-cookie';
+import TemplateOne from './templates/one'
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    // Cookies.set('reqPage', 'ac', { path: '', expires: 1 });
+    var cookie = Cookies.get('reqPage');
+    if(typeof cookie === 'string'){
+      if(cookie.length > 0) {
+        let newRoutePath = '/index/' + cookie;
+        this.props.dispatch(requestRedirect({
+          redirect: true, redirectPath: newRoutePath }
+        ))
+      }
+    } 
+  }
+  componentDidUpdate(prevProps) {
+    console.log('component did update')
+    if(this.props.redirect === true) {
+      Cookies.remove('reqPage', { path: '' });
+      this.props.dispatch(requestRedirect({
+        redirect: false, redirectPath: '/' }
+      ))
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <TemplateOne {...this.props} />
+      </Router>
+    )
+  }
+}
+function mapStateToProps(state) {
+  const { redirect, redirectPath } = state.core;
+  const { user } = state;
+  return {
+    redirect, redirectPath, user
+  }
+}
+export default connect(mapStateToProps)(App)
---
+++ index.js
@@ -0,0 +1,21 @@
+import '@babel/polyfill'
+import * as React from 'react'
+import _ from 'lodash'
+window._ = _;
+window.React = React;
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import registerServiceWorker from './lib/config/registerServiceWorker'
+import './main.css'
+import configureStore from './lib/config/store/configureStore'
+import rootSaga from './lib/infra/sagas'
+import App from './app/App'
+const store = configureStore()
+store.runSaga(rootSaga)
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app'),
+)
+registerServiceWorker();
